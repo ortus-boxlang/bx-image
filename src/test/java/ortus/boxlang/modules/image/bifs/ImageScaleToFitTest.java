293a9e27162734f0f83037d68fad956dde3f309b
Jacob Beers <jacobtbeers@gmail.com>
2025-06-19 23:30:11 -0500

    BL-1216 FIx the ImageScaleToFit BIF

--- src/test/java/ortus/boxlang/modules/image/bifs/ImageScaleToFitTest.java
+++ src/test/java/ortus/boxlang/modules/image/bifs/ImageScaleToFitTest.java
@@ -3,6 +3,9 @@ package ortus.boxlang.modules.image.bifs;
 import static com.google.common.truth.Truth.assertThat;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,32 +41,51 @@ public class ImageScaleToFitTest {
 	@DisplayName( "It should resize an image and keep the aspect ratio" )
 	@Test
 	public void testScaleToFit() throws IOException {
+		String fileName = "logo-scale-to-fit.png";
+		// @formatter:off
 		instance.executeSource( """
-		                                          result = ImageRead( "src/test/resources/logo.png" );
-		                        ImageScaleToFit( result, 100, "" );
-		                                          """, context );
+			result = ImageRead( "src/test/resources/logo.png" );
+			ImageScaleToFit( result, 100, "" );
+			ImageWrite( result, "src/test/resources/generated/%s" );
+			// ImageWrite( result, "src/test/resources/test-images/%s" )
+		""".formatted( fileName, fileName ), context );
+		// @formatter:on
 
 		assertThat( variables.get( result ) ).isInstanceOf( BoxImage.class );
 		BoxImage res = ( BoxImage ) variables.get( result );
 
 		assertThat( res.getWidth() ).isEqualTo( 100 );
 		assertThat( res.getHeight() ).isEqualTo( 100 );
+
+		var	actual		= Files.readAllBytes( Paths.get( "src/test/resources/generated/%s".formatted( fileName ) ) );
+		var	expected	= Files.readAllBytes( Paths.get( "src/test/resources/test-images/%s".formatted( fileName ) ) );
+
+		assertThat( Arrays.equals( actual, expected ) ).isTrue();
 	}
 
 	@DisplayName( "It should resize an image when called as a member function" )
 	@Test
 	public void testScaleToFitMemberInvocation() throws IOException {
+		String fileName = "logo-scale-to-fit.png";
+		// @formatter:off
 		instance.executeSource( """
-		                                                            result = ImageRead( "src/test/resources/logo.png" );
-		                                          result.scaleToFit( 100, "" );
-		                        imageWrite( result,"src/test/resources/testimage.png")
-		                                                            """, context );
+			result = ImageRead( "src/test/resources/logo.png" );
+			result.scaleToFIt( 100, "" );
+			ImageWrite( result, "src/test/resources/generated/%s" );
+			// ImageWrite( result, "src/test/resources/test-images/%s" )
+		""".formatted( fileName, fileName ), context );
+		// @formatter:on
 
 		assertThat( variables.get( result ) ).isInstanceOf( BoxImage.class );
 		BoxImage res = ( BoxImage ) variables.get( result );
 
 		assertThat( res.getWidth() ).isEqualTo( 100 );
 		assertThat( res.getHeight() ).isEqualTo( 100 );
+
+		var	actual		= Files.readAllBytes( Paths.get( "src/test/resources/generated/%s".formatted( fileName ) ) );
+		var	expected	= Files.readAllBytes( Paths.get( "src/test/resources/test-images/%s".formatted( fileName ) ) );
+
+		assertThat( Arrays.equals( actual, expected ) ).isTrue();
 	}
 
 }
